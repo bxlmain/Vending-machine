b3937d528046494092c75e41e318d4a38c12a1bf
bxlmain <clank627@gmail.com>
2024-04-15 12:18:54 +0300

    correct

--- Vending machine.cpp
+++ Vending machine.cpp
@@ -39,7 +39,7 @@ public:
     dispenserType()
     {
         numberOfItems = 50;
-        itemCost = 0;
+        itemCost = 50;
     }
 
     dispenserType(int items, int cost)
@@ -68,61 +68,66 @@ public:
 
 void showSelection() 
 {
-    cout << "1A. Сок Добрый (80 руб)" << endl;
-    cout << "2А. Шоколадный батончик Snickers (120 руб)" << endl;
-    cout << "3А. Палпи (90 руб)" << endl;
+    cout << "1. Сок Добрый (80 руб)" << endl;
+    cout << "2. Шоколадный батончик Snickers (120 руб)" << endl;
+    cout << "3. Палпи (90 руб)" << endl;
+    cout << "Введите номер товара: ";
 }
 
 void sellProduct(dispenserType& dispenser, cashRegister& cashRegister) 
 {
-    int choice;
-    int amount;
-
-    cout << "Введите номер продукта: ";
-    cin >> choice;
-
-    switch (choice) 
-    {
-    case 1:
-        cout << "Вы выбрали Сок Добрый." << endl;
-        cout << "Цена: 80 руб" << endl;
-        break;
-    case 2:
-        cout << "Вы выбрали Шоколадный батончик Snickers." << endl;
-        cout << "Цена: 120 руб" << endl;
-        break;
-    case 3:
-        cout << "Вы выбрали Палпи." << endl;
-        cout << "Цена: 90 руб" << endl;
-        break;
-    default:
-        cout << "Указан неверный номер." << endl;
-        return;
+    int cost = dispenser.getCost();
+    int balance = cashRegister.getCurrentBalance();
+    int cash;
+    if (dispenser.getNoOfItems() > 0) {
+        cout << "Стоимость товара: " << cost << "руб.\n"  << "Введите сумму(руб): ";
+        cin >> cash;
+        if (cash >= cost) {
+            if (balance >= cash - cost) {
+                dispenser.makeSale();
+                cashRegister.acceptAmount(cost);
+                cout << "Подождите, идет выдача товара.\n";
+                cout << "Ваша сдача: " << cash - cost << "руб.\n";
+            }
+            else {
+                cout << "В автомате нет сдачи \nВнесите меньшую сумму.\n";
+            }
+        }
+        else {
+            cout << "Недостаточно средств. Внесите большую сумму.\n";
+        }
     }
-
-    cout << "Введите сумму: ";
-    cin >> amount;
-
-    if (amount < dispenser.getCost()) 
-    {
-        cout << "Недостаточно средств." << endl;
-    }
-    else 
-    {
-        dispenser.makeSale(); 
-        cashRegister.acceptAmount(amount); 
-        cout << "Покупка выполнена." << endl;
+    else {
+        cout << "Товара нет в наличии:(\n";
     }
 }
 
 int main() 
 {
     setlocale(LC_ALL, "RU");
-    dispenserType dispenser;
-    cashRegister cash;
+    dispenserType juice(1, 80), chocolate(2, 120), pulpy(3,90);
+    cashRegister cash(2000);
 
+    int choice;
     showSelection();
-    sellProduct(dispenser, cash);
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        sellProduct(juice, cash);
+        break;
+    case 2:
+        sellProduct(chocolate, cash);
+        break;
+    case 3:
+        sellProduct(pulpy, cash);
+        break;
+    default:
+        cout << "Неправильный выбор.\n";
+        break;
+    }
+
 
     return 0;
 }
